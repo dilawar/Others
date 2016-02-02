d61432fcb5f91b8a068d25fb16dbd1ba7fb7a4c7
Dilawar Singh <dilawars@ncbs.res.in>
2016-02-02 17:24:34 +0530

    Shell script to run the script on directory.

---
+++ ZebraFish/run_right.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ ! -d $1 ]; then
+    echo "Directory $1 not found"
+    exit
+fi
+find $1 -type f -path "*Right*.csv" -print0 | xargs -0 -I file ./turns_in_swim.py \
+    -i file 
+

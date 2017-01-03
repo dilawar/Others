18b907ee787d535d002116864baf40c12263340b
Dilawar Singh <dilawars@ncbs.res.in>
2017-01-03 17:15:18 +0530

    Dolly headache. Not mine.. not mine.. not mine...

--- Dolly/plot.py
+++ Dolly/plot.py
@@ -16,24 +16,27 @@ import os
 import math
 import matplotlib.cm as cm
 import numpy as np
+import random
 import pandas
 from collections import defaultdict
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-# plt.style.use( 'ggplot' )
+print plt.style.available
+plt.style.use( 'seaborn-dark' )
+
 mpl.rcParams.update( { 'font.size' : 8 } )
 
+random.seed( 0 )
 
 families_ = { }
 targets_  = [ ]
+colors_ = {}
 
 def plotthis( data ):
     global families_, targets_
     families = set( data[ 'Species' ] )
     for i, family in enumerate( families ):
-        print '====='
-        print family
         items = data[ data['Species'] == family ]
         families_[ family ] = items
         [ targets_.append( x ) for x in list(items[ 'Annotation' ] ) ]
@@ -41,15 +44,15 @@ def plotthis( data ):
 
     # start plotting
     fig = plt.figure( figsize=(15, 15) )
-    ax = fig.add_axes( [0.3, 0.3, 0.4, 0.4 ], polar = True )
-    plt.grid( False )
+    pad_ = 0.25
+    ax = fig.add_axes( [0.3, 0.3, 0.5, 0.5 ], polar = True )
+    ax.grid( False )
     # ax.axis( 'off' )
-
 
     theta = 0.0
     spacing = 3
     totalSpokes = len( set(data['Strain'] ) )
-    print( 'total spokes %d' % totalSpokes )
+    print(( 'total spokes %d' % totalSpokes ))
     stepTheta = 2 * np.pi /  ( totalSpokes + spacing * len( families_) )
 
     bottom_ = 100
@@ -58,13 +61,15 @@ def plotthis( data ):
     # maxW = 150
     thetaGridPos, thetaGridLabels = [ ], [ ]
     groupPos, groupLabel = [], []
-    for family in families_:
+    legends = [ ]
+    cmap = cm.get_cmap( 'Paired', lut = 40 )
+    for family in sorted( families_, reverse = True ):
         groupLabel.append( family )
         groupPos.append( theta )
 
         group = families_[ family ]
         groupSize = len( group )
-        print( 'Goup %s size %d' % ( family, groupSize ) )
+        print(( 'Goup %s size %d' % ( family, groupSize ) ))
         # print( '\t%s' % rnas )
         allTargets = list( set( group[ 'Annotation' ] ) )
         for gi, item in enumerate( set(group[ 'Strain' ]) ):
@@ -72,16 +77,25 @@ def plotthis( data ):
             theta += stepTheta
             for i, t in enumerate(targets):
                 # print( '+ %s is targets' % t )
-                clr = cm.Paired( 7 * targets_.index( t ) )
+                label_ = '_nolegend_'
+                if t not in legends:
+                    label_ = t.decode('utf-8')
+                    legends.append( t )
+                    colors_[ t ] =  len( colors_ )
+                clr = cmap( colors_[t] )
+                # x = len( legends ) / 40.0
+                # clr = ( x, 0, x )
                 bar = ax.bar( theta
                         , spokeLen
                         , 0.5*stepTheta
                         # , bottom = bottom_ + allTargets.index( t ) * spokeLen 
                         , bottom = bottom_ + i * spokeLen 
                         , color = clr , edgecolor = clr
+                        , label = label_
                         )
+
                 proteinBar = ax.bar( theta
-                        , 3 * getAntarProteinMean( item, data )
+                        , 4 * getAntarProteinMean( item, data )
                         , 0.5 * stepTheta
                         , bottom = 50
                         , color = 'black'
@@ -93,23 +107,26 @@ def plotthis( data ):
         theta += spacing * stepTheta
 
     # Set family names
-    ax.set_yticks( [], [] )
+    ax.set_yticks( [] , [] )
     set_label( ax, thetaGridPos, thetaGridLabels )
-    maxW = 150
-    for x, p in zip( groupPos, groupLabel ):
-        l = ax.annotate( p, xy = (x, maxW), xytext = (x, maxW) )
-        ang = 180 * x / np.pi 
-        l.set_rotation( ang - 90 )
-        l.set_ha( 'left' )
-        l.set_va( 'bottom' )
+    # maxW = 150
+    # for x, p in zip( groupPos, groupLabel ):
+        # l = ax.annotate( p, xy = (x, maxW), xytext = (x, maxW) )
+        # ang = 180 * x / np.pi 
+        # l.set_rotation( ang - 90 )
+        # l.set_ha( 'left' )
+        # l.set_va( 'bottom' )
         # if ang > 90 and ang < 180:
             # l.set_va( 'bottom' )
         # if ang < 180:
             # l.set_va( 'bottom' )
         # elif ang > 180:
             # l.set_va( 'top' )
-        print x, p
+        # print x, p
     print( 'Saved to result.png' )
+    plt.legend( loc = 2, framealpha = 0.0
+            , bbox_to_anchor = (-0.6, 0.4 )
+            )
     plt.savefig( 'result.png' )
 
 def getAntarProteinMean( s, data ):
@@ -129,8 +146,6 @@ def set_label( ax, pos, labels, frac = 1.1 ):
         elif ang > 180:
             l.set_va( 'top' )
         
-
-
 def main():
     """docstring for main"""
     data = pandas.read_table( sys.argv[1] )

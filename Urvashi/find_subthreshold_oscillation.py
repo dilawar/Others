49d9a526875921dba8d9a6f233bf82e880599ec1
Dilawar Singh <dilawars@ncbs.res.in>
2019-05-04 17:50:10 +0530

    It is in good shape.

--- Urvashi/find_subthreshold_oscillation.py
+++ Urvashi/find_subthreshold_oscillation.py
@@ -14,13 +14,14 @@ __status__           = "Development"
 import sys
 import os
 import pyabf
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 try:
-    plt.style.use('seaborn-talk')
+    #mpl.style.use( ['bmh', 'fivethirtyeight'] )
+    mpl.style.use( 'ggplot')
 except Exception as e:
     pass
 import scipy.fftpack as fft
-import pywt
 import analytic_wfm
 import scipy.signal
 import numpy as np
@@ -29,14 +30,6 @@ import pandas as pd
 fs_ = 20e3
 dt_ = 1.0/fs_
 
-plt.figure(figsize=(15, 8))
-gridSize = (3, 3)
-ax1 = plt.subplot2grid(gridSize, (0,0), colspan=2)
-axFFT = plt.subplot2grid(gridSize, (0,2), rowspan=1)
-axFFT1 = plt.subplot2grid(gridSize, (1,2), rowspan=1)
-ax2 = plt.subplot2grid( gridSize, (1,0), colspan=2)
-
-#  axImgImg = plt.subplot2grid( gridSize, (2,1), colspan=1, rowspan=2)
 
 def abf_to_dfs(files):
     dfs = {}
@@ -54,8 +47,34 @@ def abf_to_dfs(files):
     return dfs
 
 
-def plot_axis(ax, x, y):
-    ax.plot(x, y)
+def plot_time_series(x, y, drive, peaks, outfile):
+    #  plt.figure(figsize=(15, 10))
+    gridSize = (2, 3)
+    ax1 = plt.subplot2grid(gridSize, (0,0), colspan=2)
+    axFFT1 = plt.subplot2grid(gridSize, (0,2), rowspan=1)
+    ax2 = plt.subplot2grid( gridSize, (1,0), colspan=2, sharex=ax1)
+    axFs = plt.subplot2grid(gridSize, (1,2), rowspan=1)
+
+    ax1.plot(x, y)
+    axFFT1.psd(y, Fs=fs_)
+    axFFT1.set_xscale('log')
+    axFFT1.set_ylabel('PSD')
+
+
+    ax2.plot(x, drive, lw=0.5)
+    ax2.set_xlabel('Time (sec)')
+    xP, yP = zip(*peaks)
+    ax2.plot(xP, yP, 'k.')
+
+    # Plot the histogram of frequencies.
+    ts = np.diff(xP)
+    fs = 1/ts
+    axFs.hist(fs, bins=np.arange(fs.min(), fs.max(), 1))
+    axFs.set_xlabel('Frequency (Hz)')
+    
+
+    plt.tight_layout()
+    plt.savefig( outfile )
 
 def butter_bandpass(lowcut=0.001, highcut=1000, order=6):
     nyq = 0.5 * fs_
@@ -85,79 +104,48 @@ def do_cwt(t, y, ax):
     ax.contourf(T, S, power, 100)
     #  ax.set_yscale('log')
 
-def analyze(x, y):
-    ax1.plot(x, y, alpha=0.8)
+def analyze_time_series(x, y):
     y1 = y.copy()
-    thres = y1.mean() #+ y1.std()
-    #  y1[y1<thres] = thres
     y1 = y1 - y1.min()
     y1 = y1 / y1.max()
-
-    axFFT.psd(y, NFFT=2048, Fs=fs_, noverlap=56) #, detrend='linear')
-    axFFT.set_xscale('log')
-
-    # Band pass
-    extract_osci(x, y1)
+    return extract_osci(x, y1)
 
 def extract_osci(x, y):
     sos = butter_bandpass(lowcut=10, highcut=100, order=20)
     ySmooth = scipy.signal.sosfilt(sos, y)
     ySmooth -= ySmooth.mean()
     ySmooth = ySmooth / ySmooth.max()
-    ax2.plot(x, ySmooth, alpha=0.8)
 
     # peak detection. Anything faster than 10ms is bogus.
     drive = smooth(ySmooth, 20e-3)
     drive = drive - drive.mean()
     drive = drive / max(drive.max(), -drive.min())
-    ax2.plot(x, drive)
+    drive[drive < 0.5*drive.std()] = 0
     maxP, minP = analytic_wfm.peakdetect(drive, x, delta=10e-3)
     pI, pV = zip(*maxP)
-    ax2.plot(pI, pV, 'k.')
 
     # Estimate frequency.
     driveT = np.diff(pI)
-    print(driveT)
-    print(np.mean(driveT))
+    driveF = 1.0 / driveT
+    return drive, maxP, driveF
 
-    
-    #  yMoreSmooth = yMoreSmooth - yMoreSmooth.mean()
-    #  u, s = np.mean(yMoreSmooth), np.std(yMoreSmooth)
-    #  thres = u + 0.5*s
-    #  yMoreSmooth[(yMoreSmooth > thres)] = 1
-    #  yMoreSmooth[(yMoreSmooth < -thres)] = -1
-    #  yMoreSmooth[yMoreSmooth < 0.0 ] = -1e-2
-    #  drive = yMoreSmooth
-    #  ax2.plot(x, drive, alpha=0.8)
-
-    axFFT1.psd(ySmooth, Fs=fs_)
-    axFFT1.set_xscale('log')
-
-def process(df):
+def process(df, f):
     tvec = df['Time']
-    y1 = df['Trace 0']
-    if True:
-        a = int(32.2/dt_)
-        b = a+60000
-        x, y = tvec[a:b], y1[a:b]
-    else:
+    plot = True
+    for c in df.columns:
+        if c == 'Time':
+            continue
+        y1 = df[c]
         x, y = tvec, y1
-    analyze(x, y)
-
-    #plot_axis(ax1, tvec, y1)
-
-    #y1[ y1 <= np.mean(y1)] = np.mean(y1)
-    #y1 = smooth(y1, 200e-3)
-    #plot_axis(ax2, tvec, y1)
-
-    plt.tight_layout()
-    plt.savefig(f'{__file__}.png')
+        drive, peaks, fs = analyze_time_series(x, y)
+        if plot:
+            outfile = f"{f}.{c}.png"
+            plot_time_series(x, y, drive, peaks, outfile)
 
 def main(args):
     dfs = abf_to_dfs(args.input_abf)
     for f, df in dfs.items():
-        print(df.columns)
-        process(df)
+        process(df, f)
 
 if __name__ == '__main__':
     import argparse
